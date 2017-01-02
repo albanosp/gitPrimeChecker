d0af61e5b64481280803fe3e6179547debc01f3e
albanosp <steve.albano@gmail.com>
2017-01-02 17:37:12 -0500

    working copy

--- gitPrimeChecker/ViewController.swift
+++ gitPrimeChecker/ViewController.swift
@@ -13,18 +13,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var promptLabel: UILabel!
     @IBOutlet weak var numberToCheck: UITextField!
     @IBOutlet weak var resultDisplay: UILabel!
-
-    @IBAction func checkPrime(_ sender: Any) {
     
-        if let checkMe = numberToCheck {
-            resultDisplay.textColor = UIColor.green
-            resultDisplay.text = "\(checkMe.text!) is Prime"
-        }   else {
-            resultDisplay.textColor = UIColor.red
-            resultDisplay.text = "Number is not Prime"
+    var tally = 0
+    
+    @IBAction func checkPrime(_ sender: Any)
+        {
+        let checkUserInput = Int(numberToCheck.text!)
+        if var count = checkUserInput
+        {
+            if count < 2
+            {
+            count = 0
+            tally = 4
+            }
+            while count > 0
+            {
+                if checkUserInput! % count == 0
+                {
+                    tally += 1
+                }
+                count -= 1
+            }
+            if tally > 2
+            {
+                tally = 0
+                resultDisplay.textColor = UIColor.red
+                resultDisplay.text = "\(checkUserInput!) is NOT Prime"
+            }
+            else {
+                tally = 0
+                resultDisplay.textColor = UIColor.green
+                resultDisplay.text = "\(checkUserInput!) IS Prime"
+                }
+            }
+            else {
+                resultDisplay.textColor = UIColor.red
+                resultDisplay.text = "Input is not a valid integer"
 
+                }
         }
-    }
 
     
     override func viewDidLoad() {
